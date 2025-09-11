8d1ff89238594a11867e091f78148979f9cd2e68
MICHAEL MEEROVICH <a61d3799-dd1f-4e7c-ab20-c644ecf612ec@KAM-CA-H4TG50HKQ6XD.local>
2025-09-11 14:00:58 -0400

    text

--- src/main/java/FizzBuzz.java
+++ src/main/java/FizzBuzz.java
@@ -8,7 +8,26 @@ public class FizzBuzz {
      * - Otherwise -> the number itself
      * Output should be 100 whitespace-separated tokens (spaces or newlines are fine).
      */
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         // TODO
+        for (int k = 0; k <= 100; ++k)
+        {
+            if (k % 3 == 0 && k % 5 == 0)
+            {
+                System.out.println("FizzBuzz");
+            }
+            else if (k % 5 == 0)
+            {
+                System.out.println("Buzz");
+            }
+            else if (k % 3 == 0) {
+                System.out.println("Fizz");
+            }
+            else
+            {
+                System.out.println(k);
+            }
+        }
     }
 }
